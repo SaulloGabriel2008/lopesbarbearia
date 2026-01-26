9083ef9acf59f85069e692f22fef842e06a08125
SaulloGabriel2008 <saullonegocios@gmail.com>
2026-01-26 19:53:51 -0300

    Add files via upload

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,25 +1,23 @@
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "SUA_API_KEY",
-  authDomain: "barbearia-do-rei.firebaseapp.com",
-  projectId: "barbearia-do-rei",
-  storageBucket: "barbearia-do-rei.firebasestorage.app",
-  messagingSenderId: "546744214164",
-  appId: "1:546744214164:web:5bd367381afbef253d0a81"
+            apiKey: "AIzaSyBLBFTLC7sqM9VUhlsypPugQ-LhcX7-0Rw",
+            authDomain: "barbearia-do-rei.firebaseapp.com",
+            projectId: "barbearia-do-rei",
+            storageBucket: "barbearia-do-rei.firebasestorage.app",
+            messagingSenderId: "546744214164",
+            appId: "1:546744214164:web:5bd367381afbef253d0a81"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  console.log("📩 Notificação recebida em background:", payload);
-
+messaging.onBackgroundMessage(payload => {
   self.registration.showNotification(
     payload.notification.title,
     {
       body: payload.notification.body,
-      icon: "/icons/icon-192.png"
+      icon: "/icon-192.png"
     }
   );
 });
